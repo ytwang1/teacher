a893dc18cae94331905e7d83e11cd8ecafd19784
ytwang1 <yangting_wang1@163.com>
2017-10-19 17:43:23 +0800

    worked version generate phone,name

--- src/com/netinnet/action/generateRandomInfo.java
+++ src/com/netinnet/action/generateRandomInfo.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 public class generateRandomInfo implements ActionClass {
     public ActionClassResultDTO execute(HttpServletRequest request) {
 
+        //随机生成手机号码
         for (int i = 0; i < 11; i++) {//11代表循环11次，产生11个随机号码
             String randomPhoneNo = "13";//定义电话号码以13开头
             Random random = new Random();//定义random，产生随机数
@@ -20,7 +21,8 @@ public class generateRandomInfo implements ActionClass {
             RequestUtil.addVar(request, "teacherRandomPhone", randomPhoneNo);
         }
 
-        for (int i=0;i< 10;i++){String[] name = {"Carol", "Ruth", "Sharon", "Michelle", "Laura", "James", "John", "Robert", "Michael"};
+        //随机生成名字
+        for (int i=0;i< 20;i++){String[] name = {"Carol", "Ruth", "Sharon", "Michelle", "Laura", "Shirley", "Cynthia", "Angela", "Melissa", "Brenda","James", "John", "Robert", "Michael","Jose", "Larry", "Jeffrey", "Frank", "Scott"};
             int a = (int) Math.abs(name.length * Math.random());
             String randomName= name[a];
             RequestUtil.addVar(request, "teacherRandomName", randomName);

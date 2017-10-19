8dbd94c80021171cd4bca463ac928524482b5a91
ytwang1 <yangting_wang1@163.com>
2017-10-19 16:39:48 +0800

    randomPhone 11 numbers

--- src/com/netinnet/action/test.java
+++ src/com/netinnet/action/test.java
@@ -3,6 +3,7 @@ package com.netinnet.action;
 import cn.netinnet.qdk.action.ActionClass;
 import cn.netinnet.qdk.action.ActionClassResultDTO;
 import cn.netinnet.qdk.util.RequestUtil;
+import java.util.Random;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,11 +13,19 @@ public class test implements ActionClass {
         /*
 
          */
-        String randomPhoneNo="9999999999";
+        for (int i = 0; i < 11; i++) {//11代表循环11次，产生11个随机号码
+            String randomPhoneNo = "13";//定义电话号码以13开头
+            Random random = new Random();//定义random，产生随机数
+            for (int j = 0; j < 9; j++) {
+                //生成0~9 随机数
+                randomPhoneNo += random.nextInt(9);
+            }
+            RequestUtil.addVar(request, "teacherPhone222", randomPhoneNo);
+        }
         /*
 
          */
-        RequestUtil.addVar(request, "teacherPhone222", randomPhoneNo);
+
         return new ActionClassResultDTO();
     }
 }
